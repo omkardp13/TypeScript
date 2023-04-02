53ae38d50b7300fb131976ec456a2bf45f725d53
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2023-04-02 12:15:37 +0530

    Update Default.ts

--- Functional_Parameters/Default.ts
+++ Functional_Parameters/Default.ts
@@ -1,3 +1,6 @@
+//Default parameters in TypeScript are a feature that allows developers to provide default values for function parameters. 
+//This means that if a function is called without providing a value for a certain parameter, the default value will be used instead.
+
 function Percentage(Marks:number=300,OutOf:number=500):number
 {
   let Ans:number=0;
