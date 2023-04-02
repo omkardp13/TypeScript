8210d447681f456e4f4c80be43808cd2c2555fee
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2023-04-02 12:24:17 +0530

    Update Positional.ts

--- Functional_Parameters/Positional.ts
+++ Functional_Parameters/Positional.ts
@@ -1,4 +1,5 @@
-
+//Positional parameters in TypeScript are the standard parameters that are passed to a function in a specific order.
+//Positional parameters are defined in the function signature and correspond to the arguments passed to the function in the order they are listed.
 
 function Addition(No1 :number,No2:number):number
 {
