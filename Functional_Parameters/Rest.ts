439b1abf4a45dd9916e115e290f6552d1aa32b18
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2023-04-02 12:26:57 +0530

    Update Rest.ts

--- Functional_Parameters/Rest.ts
+++ Functional_Parameters/Rest.ts
@@ -1,4 +1,9 @@
 
+//In TypeScript, a rest parameter is denoted by using the ellipsis ... followed by the name of the parameter. 
+//A rest parameter allows a function to accept an indefinite number of arguments as an array.
+//Note that the rest parameter must be the last parameter in the function's parameter list.
+
+
 function Addition(...Arr:number[]):number
 {
 	let Sum:number=0;
