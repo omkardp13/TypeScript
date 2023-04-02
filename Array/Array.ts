9d4e3d33234ca4239b2fa4d88150953c1264786c
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2023-04-02 12:36:21 +0530

    Add files via upload

---
+++ Array/Array.ts
@@ -0,0 +1,26 @@
+
+var Monday:number=560;
+var Tuesday:number=900;
+var Wednesday:number=320;
+var Thursday:number=660;
+var Friday:number=220;
+var Saturday:number=1320;
+var Sunday:number=470;
+
+//Syntax 1
+
+var Week1:number[]=[560,900,320,660,220,1320,470];
+
+//Syntax 2
+
+var Week2:number[];
+Week2=[560,900,320,660,220,1320,470];
+
+//Syntax3
+
+var Week3:number[]=new Array(7);
+Week3=[560,900,320,660,220,1320,470];
+
+//Syntax
+var Batches:number=new Array("PPA","LB","Angular","Python","LSP");
+
